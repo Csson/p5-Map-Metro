9be89d257fcf7f63caa4a1a40a2fed11cebefcc5
Erik Carlsson <info@code301.com>
2014-12-05 22:56:53 +0100

    "Init."

---
+++ lib/Map/Metro.pm
@@ -0,0 +1,28 @@
+package Map::Metro;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Map::Metro - Short intro
+
+=head1 SYNOPSIS
+
+  use Map::Metro;
+
+=head1 DESCRIPTION
+
+Map::Metro is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More;
+use Map::Metro;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
+
