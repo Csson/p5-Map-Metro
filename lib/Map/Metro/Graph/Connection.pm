0c61ac744732ac7326c1bf4fb7e51d999406f9cb
Erik Carlsson <info@code301.com>
2014-12-09 20:06:59 +0100

    Since routes no longer has connections, connections need no methods

--- lib/Map/Metro/Graph/Connection.pm
+++ lib/Map/Metro/Graph/Connection.pm
@@ -28,65 +28,6 @@ class Map::Metro::Graph::Connection using Moose {
         required => 1,
         default => 1,
     );
-        has id => (
-        is => 'ro',
-        isa => Str,
-        init_arg => undef,
-        default => sub { join '' => map { ('a'..'z', 2..9)[int rand 33] } (1..8) },
-    );
-
-    method is_line_transfer {
-     #   say sprintf '   line:  %s - %s, [lsid: %s - %s] sid: %s - %s    %s/%s', $self->origin_line_station->line->id,
-     #                                                                           $self->destination_line_station->line->id,
-     #                                                                           $self->origin_line_station->line_station_id,
-     #                                                                           $self->destination_line_station->line_station_id,
-     #                                                                           $self->origin_line_station->station->id,
-     #                                                                           $self->destination_line_station->station->id,
-     #                                                                           $self->origin_line_station->station->name,
-     #                                                                           $self->destination_line_station->station->name;
-     #   say sprintf '     next: %s', $self->next_connection->origin_line_station->station->name if $self->next_connection;
-
-        return $self->origin_line_station->station->id == $self->destination_line_station->station->id;
-        return $self->origin_line_station->line->id ne $self->destination_line_station->line->id;
-    }
-    method is_station_transfer {
-        my $origin_station_line_ids = [ map { $_->id } $self->origin_line_station->station->all_lines ];
-        my $destination_station_line_ids = [ map { $_->id } $self->destination_line_station->station->all_lines ];
-
-        my $are_on_same_line = List::Compare->new($origin_station_line_ids, $destination_station_line_ids)->get_intersection;
-
-        return !$are_on_same_line;
-    }
-    method was_line_transfer {
-        return if !$self->has_previous_connection;
-        return $self->previous_connection->is_line_transfer;
-    }
-    method was_station_transfer {
-        return if !$self->has_previous_connection;
-        #return $self->previous_connection->origin_line_station->station->id == $self->origin_line_station->station->id;
-        return $self->previous_connection->is_station_transfer;
-    }
-
-    method to_text(Int $line_name_length = 0) {
-        my @rows = ();
-
-        push @rows =>  sprintf "[ %1s %-${line_name_length}s ] %s" => ($self->was_line_transfer && !$self->was_station_transfer ? '*' : ''),
-                                                                       $self->origin_line_station->line->name,
-                                                                       $self->origin_line_station->station->name;
-        if($self->is_station_transfer) {
-            push @rows =>  sprintf "[ %1s %-${line_name_length}s ] %s" => ($self->is_station_transfer ? '+' : ''),
-                                                    ' ' x length $self->origin_line_station->line->name,
-                                                    $self->destination_line_station->station->name;
-        }
-        if(!$self->has_next_connection) {
-            push @rows =>  sprintf "[ %1s %-${line_name_length}s ] %s" => '',
-                                                                     $self->destination_line_station->line->name,
-                                                                     $self->destination_line_station->station->name;
-        }
-        return join "\n" => @rows;
-    }
-
-
 }
 
 __END__
