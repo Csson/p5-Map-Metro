3be5f87efd49ebbef84347ee359097d98ab52837
Erik Carlsson <info@code301.com>
2014-12-09 21:22:08 +0100

    Cleanup

--- lib/Map/Metro/Cmd/AllRoutes.pm
+++ lib/Map/Metro/Cmd/AllRoutes.pm
@@ -19,37 +19,9 @@ class Map::Metro::Cmd::AllRoutes extends Map::Metro::Cmd using Moose {
         my $graph = $self->cityname !~ m{\.} ? Map::Metro->new($self->cityname)->parse : Map::Metro::Shim->new($self->cityname)->parse;
         my $all = $graph->all_pairs;
 
-        say 'GET READY';
-
-        sleep 5;
-
-        say 'GET READIER';
-
-        sleep 5;
-
-        say 'NOW....';
-
-
-        sleep 2;
-
-        my $start = 4;
-        my $end = 56;
-        for my $i (1..20) {
-        try {
-            $start += $i;
-            $end += $i;
-            my $routing = $graph->routes_for($start, $end);
-            say $routing->to_text;
+        foreach my $route ($all->@*) {
+            say $route->to_text;
         }
-        catch {
-
-        };
-    }
-
-
-        #foreach my $route ($all->@*) {
-        #    say $route->to_text;
-        #}
     }
 }
 
