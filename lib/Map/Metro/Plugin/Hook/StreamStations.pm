47cb4ccf67fa9e008e793cf94009cf3cf6c87fb0
Erik Carlsson <info@code301.com>
2014-12-12 22:26:40 +0100

    Expanded StreamStations to keep the station names for later

--- lib/Map/Metro/Plugin/Hook/StreamStations.pm
+++ lib/Map/Metro/Plugin/Hook/StreamStations.pm
@@ -2,12 +2,27 @@ use feature ':5.20';
 
 package Map::Metro::Plugin::Hook::StreamStations {
 
+    use Moose;
+    use Types::Standard -types;
+
+    has station_names => (
+        is => 'rw',
+        isa => ArrayRef,
+        traits => ['Array'],
+        handles => {
+            add_station_name => 'push',
+            all_station_names => 'elements',
+            get_station_name => 'get',
+        },
+    );
+
     sub register {
         before_add_station => sub {
             my $self = shift;
             my $station = shift;
 
             say $station->name;
+            $self->add_station_name($station->name);
         };
     }
 }
