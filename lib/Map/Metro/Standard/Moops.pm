0ade5fdf07701e2b74a016ffbb328ce71498b0f3
Erik Carlsson <info@code301.com>
2014-12-13 01:58:55 +0100

    Suppresses warnings?

--- lib/Map/Metro/Standard/Moops.pm
+++ lib/Map/Metro/Standard/Moops.pm
@@ -20,7 +20,7 @@ package Map::Metro::Standard::Moops {
             'String::Trim'      => ['trim'],
             'feature'           => [qw/:5.20/],
             'experimental'      => [qw/postderef/],
-            'Map::Metro::Types' => ['-types', { replace => 1 }],
+            'Map::Metro::Types' => [{ replace => 1 }, '-types'],
             'List::Compare'     => [],
             'MooseX::SetOnce'   => [],
         );
